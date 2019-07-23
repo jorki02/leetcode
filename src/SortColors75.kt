b8bd30430d708ef7291c3cd1e7ee07ff638ab690
Alex <jorki02@gmail.com>
2019-07-23 18:45:04 +0300

    SortColors75

---
+++ src/SortColors75.kt
@@ -0,0 +1,76 @@
+import java.util.*
+
+class Solution {
+    fun sortColors(nums: IntArray): Unit {
+        if(nums.isEmpty()) return
+
+        val size = nums.size
+
+        var start = 0
+        var end = size - 1
+
+        while(start < size && nums[start] == 0) start++
+        while(end >= 0 && nums[end] == 2) end--
+
+        for(i in 0 until nums.size){
+            if(i < start){
+                continue
+            }
+
+            if(start > end){
+                break
+            }
+
+            if(i > end){
+                break
+            }
+
+            if(nums[i] == 1){
+                continue
+            }
+
+            if(nums[i] == 0){
+                val temp = nums[start]
+                nums[start] = nums[i]
+                nums[i] = temp
+                start++
+                while(start < size && nums[start] == 0) start++
+
+                if(nums[i] == 2){
+                    val temp = nums[end]
+                    nums[end] = nums[i]
+                    nums[i] = temp
+                    end--
+                    while(end >= 0 && nums[end] == 2) end--
+                }
+            }
+
+            if(nums[i] == 2){
+                val temp = nums[end]
+                nums[end] = nums[i]
+                nums[i] = temp
+                end--
+                while(end >= 0 && nums[end] == 2) end--
+
+                if(nums[i] == 0) {
+                    val temp = nums[start]
+                    nums[start] = nums[i]
+                    nums[i] = temp
+                    start++
+                    while(start < size && nums[start] == 0) start++
+                }
+            }
+        }
+    }
+
+}
+
+fun main(){
+    val solution = Solution()
+
+    val array = intArrayOf(0)
+
+    solution.sortColors(array)
+
+    println(Arrays.toString(array))
+}
